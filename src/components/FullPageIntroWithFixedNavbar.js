60d369c614d4b941f02aab376a475acca16f788a
marvin mante <marvinsjsu@gmail.com>
2018-07-05 22:36:17 -0700

    got the layout working

--- src/components/FullPageIntroWithFixedNavbar.js
+++ src/components/FullPageIntroWithFixedNavbar.js
@@ -35,10 +35,19 @@ class FullPageIntroWithFixedNavbar extends React.Component {
         collapse: false,
         isWideEnough: false,
         page: 'home',
+        form: {
+          name: '',
+          email: '',
+          message: '',
+        },
+        form_error: false,
+        form_sent: false,
       };
 
     this.onClick = this.onClick.bind(this);
     this.navClick = this.navClick.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   onClick(){
@@ -48,19 +57,45 @@ class FullPageIntroWithFixedNavbar extends React.Component {
   }
 
   navClick(currentPage){
-    console.log(currentPage);
     this.setState({
       page: currentPage,
     });
   }
 
-  render() {
+  handleChange(event) {
+    let form_id = event.target.id;
+    let form_value = event.target.value;
 
-    const mainView = {
-      background: `url(${mainBannerImage}) no-repeat center center`,
-      backgroundSize: 'cover',
-      height: '80vh'
-    };
+    this.setState(currentState => {
+      let form = currentState.form;
+      form[form_id] = form_value;
+      return { form };
+    });
+  }
+
+  sendMessage(event) {
+    console.log(event);
+    event.preventDefault();
+
+    let show_error = false;
+    // check for name, email, and message values
+    if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(this.state.form.email)) {
+      show_error = true;
+    }
+
+    if (this.state.form.name == '' || this.state.form.message == '') {
+      show_error = true;
+    }
+
+    // if all valid, make api call
+    if (!show_error) {
+      this.setState(currentState => ({
+        form_error: true,
+      }));
+    }
+  }
+
+  render() {
 
     // const mainBackground = {
     //   background: `url(${mainBackgroundImage}) no-repeat bottom center`,
@@ -74,9 +109,21 @@ class FullPageIntroWithFixedNavbar extends React.Component {
 
     const logo = {padding: 0, margin: 0, width: 100, borderWidth: 2, borderColor: '#d6d7da'};
     const navbar = {width: 105, textAlign: 'center', borderRadius: 20};
-    const cardStack = {display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap'}
+    // const cardStack = {display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap'};
+    const cardStack = {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      alignItems: 'baseline',
+      flexWrap: 'wrap',
+      minHeight: '100vh',
+      paddingTop: '100px',
+    };
+
+
     const cards = {width: 300, height: 550, margin: 20};
-    const cardsAlt = {width: 300, height: 500, margin: 20, marginTop: 100};
+    const cardsAlt = {width: 300, height: 500, margin: 20};
     const singleCard = {width: '90%', margin: 20};
     const cardImage = {minHeight: 225};
     const cardTitle = {color: '#2386C3', textAlign: 'center'};
@@ -86,22 +133,7 @@ class FullPageIntroWithFixedNavbar extends React.Component {
     const leftLogo = {width: 200, marginLeft: 10};
     const navLink = {color: '#11284c', fontWeight: '400', fontSize: 18};
     const navLinkActive = {color: '#2386C3', fontWeight: '700', fontSize: 20};
-    const mainStatement = {color: '#11284c', margin: 100, padding: 20, fontSize: 28};
-    const aboutUs = {
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-    };
 
-    const aboutUsText = {
-      background: `url(${cardImage4}) no-repeat left center`,
-      backgroundSize: '300px',
-      height: '100vh',
-      fontSize: 25,
-      fontFamily: 'Helvetica',
-      color: '#2386C3',
-      textAlign: 'left',
-    };
 
     const bannerImage = {
       width: '50%',
@@ -128,21 +160,27 @@ class FullPageIntroWithFixedNavbar extends React.Component {
     };
 
     const servicesBanner = {
-      minHeight: '100vh',
-      paddingTop: '7%',
-      marginTop: '50px',
+      // minHeight: '100vh',
+      // paddingTop: '7%',
+      // marginTop: '50px',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      alignContent: 'space-between',
+      width: '100%',
     };
 
     const contactSection = {
       display: 'flex',
       flexDirection: 'row',
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       flexWrap: 'wrap',
+      minHeight: '100vh',
       alignItems: 'center',
       alignContent: 'space-between',
-      minHeight: '100vh',
       margin: 0,
-      backgroundColor: '#e3f2fd',
     };
 
     const homeBannerImage = {
@@ -164,31 +202,68 @@ class FullPageIntroWithFixedNavbar extends React.Component {
       fontFamily: 'Helvetica',
       textAlign: 'left',
       color: '#2386C3',
-
     };
 
     const contactCard = {
+      width: '40%',
+      minWidth: 300,
       padding: '10px',
+      margin: '10px',
+      marginRight: '40px',
     };
 
     const contactForm = {
-      width: 500,
-      minHeight: 500,
+      width: '100%',
     };
 
     const contactText = {
-      display: 'flex',
-      alignItems: 'flex-start',
-      width: '50%',
-      minWidth: 350,
-      paddingBottom: '5%',
-      paddingRight: '5%',
+      width: '45%',
+      minWidth: 300,
       margin: 0,
-      fontSize: 26,
+      fontSize: 32,
       fontFamily: 'Helvetica',
       textAlign: 'left',
+      padding: '20px',
+      paddingLeft: '50px',
+      marginLeft: '50px',
+      fontWeight: '500',
       color: '#2386C3',
     };
+
+    const contactView = {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      justifyContent: 'flex-start',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      alignContent: 'space-between',
+      minHeight: '100vh',
+      background: `url(${mainBackgroundImage}) no-repeat bottom center`,
+      backgroundSize: 'cover',
+      paddingBottom: '20px',
+    };
+
+    const copyright = {
+      height: '50px',
+      paddingTop: '20px',
+      textAlign: 'center',
+      fontSize: 14,
+      fontWeight: '500',
+      color: '#2386C3',
+      backgroundColor: '#eee',
+    };
+
+    const maskView = {
+      minHeight: '100vh',
+    }
+
+    const viewContact = {
+      display: 'flex',
+      flexDirection: 'row',
+
+    };
+
 
     return (
       <div className="container-fluid" style={mainContainer}>
@@ -251,102 +326,129 @@ class FullPageIntroWithFixedNavbar extends React.Component {
             </p>
         </div>
 
-        <div id='services' style={servicesBanner}>
-            <View style={cardStack}>
-              <Card style={cards} cascade>
-                  <CardImage style={cardImage} className="img-fluid" src={cardImage1} />
-                  <CardBody>
-                      <CardTitle style={cardTitle}>Network Cabling</CardTitle>
-                      <CardText style={cardText}>
-                        <dl style={list}>
-                          <dd>Fiber Optic Installation, Fiber Optic Fusion splicing, Fiber Optic OTDR Testing</dd>
-                          <dd>Wire Additions, Wire & Cable Relocation, Patch Panel Terminations, Cat 6 Wiring</dd>
-                          <dd>
-                            Cabling for paging systems and security networks: security camera
-                            video surveillance CCTV systems
-                          </dd>
-                        </dl>
-                      </CardText>
-                  </CardBody>
-              </Card>
+        <div id='services' style={cardStack}>
+          <Card style={cards} cascade>
+              <CardImage style={cardImage} className="img-fluid" src={cardImage1} />
+              <CardBody>
+                  <CardTitle style={cardTitle}>Network Cabling</CardTitle>
+                  <CardText style={cardText}>
+                    <dl style={list}>
+                      <dd>Fiber Optic Installation, Fiber Optic Fusion splicing, Fiber Optic OTDR Testing</dd>
+                      <dd>Wire Additions, Wire & Cable Relocation, Patch Panel Terminations, Cat 6 Wiring</dd>
+                      <dd>
+                        Cabling for paging systems and security networks: security camera
+                        video surveillance CCTV systems
+                      </dd>
+                    </dl>
+                  </CardText>
+              </CardBody>
+          </Card>
 
-              <Card style={cardsAlt} cascade>
-                  <CardImage style={cardImage} className="img-fluid" src={cardImage2} />
-                  <CardBody>
-                      <CardTitle style={cardTitle}>Design and Build</CardTitle>
-                      <CardText style={cardText}>
-                        <dl style={list}>
-                          <dd>Telecommunications & Equipment Rooms, Ladder Rack & Cable Tray Installation, Cable Management</dd>
-                          <dd>Voice and Data Projects, Colocation build out and Data Center deployment</dd>
-                        </dl>
-                      </CardText>
-                  </CardBody>
-              </Card>
+          <Card style={cardsAlt} cascade>
+              <CardImage style={cardImage} className="img-fluid" src={cardImage2} />
+              <CardBody>
+                  <CardTitle style={cardTitle}>Design and Build</CardTitle>
+                  <CardText style={cardText}>
+                    <dl style={list}>
+                      <dd>Telecommunications & Equipment Rooms, Ladder Rack & Cable Tray Installation, Cable Management</dd>
+                      <dd>Voice and Data Projects, Colocation build out and Data Center deployment</dd>
+                    </dl>
+                  </CardText>
+              </CardBody>
+          </Card>
 
-              <Card style={cards} cascade>
-                  <CardImage  style={cardImage}style={cardImage} className="img-fluid" src={cardImage3}  />
-                  <CardBody>
-                      <CardTitle style={cardTitle}>Voice and data</CardTitle>
-                      <CardText style={cardText}>
-                        <dl style={list}>
-                          <dd>
-                            Converged networks and VoIP services – integrating voice,
-                            data and video communications (wired or wireless)
-                          </dd>
-                          <dd>
-                            Wireless installation and network design: access point, WiFi, RFID, WLAN, Broadband
-                          </dd>
-                          <dd>
-                            Wireless / WiFi, WiMax, Point to Point / Point to Multi-Point Wireless,
-                            WiFi Hotspots and more
-                          </dd>
-                        </dl>
-                      </CardText>
-                  </CardBody>
-              </Card>
+          <Card style={cards} cascade>
+              <CardImage  style={cardImage}style={cardImage} className="img-fluid" src={cardImage3}  />
+              <CardBody>
+                  <CardTitle style={cardTitle}>Voice and data</CardTitle>
+                  <CardText style={cardText}>
+                    <dl style={list}>
+                      <dd>
+                        Converged networks and VoIP services – integrating voice,
+                        data and video communications (wired or wireless)
+                      </dd>
+                      <dd>
+                        Wireless installation and network design: access point, WiFi, RFID, WLAN, Broadband
+                      </dd>
+                      <dd>
+                        Wireless / WiFi, WiMax, Point to Point / Point to Multi-Point Wireless,
+                        WiFi Hotspots and more
+                      </dd>
+                    </dl>
+                  </CardText>
+              </CardBody>
+          </Card>
 
-              <Card style={cardsAlt} cascade>
-                  <CardImage style={cardImage} className="img-fluid" src={cardImage4}  />
-                  <CardBody>
-                      <CardTitle style={cardTitle}>Existing Cabling Systems</CardTitle>
-                      <CardText style={cardText}>
-                        <dl style={list}>
-                          <dd>
-                            Upgrading Existing Systems, Wire Identification and Labeling
-                          </dd>
-                          <dd>
-                            Post Work Documentation, Line Testing
-                          </dd>
-                          <dd>Point of sales (POS) systems installation</dd>
-                        </dl>
-                      </CardText>
-                  </CardBody>
-              </Card>
-            </View>
+          <Card style={cardsAlt} cascade>
+              <CardImage style={cardImage} className="img-fluid" src={cardImage4}  />
+              <CardBody>
+                  <CardTitle style={cardTitle}>Existing Cabling Systems</CardTitle>
+                  <CardText style={cardText}>
+                    <dl style={list}>
+                      <dd>
+                        Upgrading Existing Systems, Wire Identification and Labeling
+                      </dd>
+                      <dd>
+                        Post Work Documentation, Line Testing
+                      </dd>
+                      <dd>Point of sales (POS) systems installation</dd>
+                    </dl>
+                  </CardText>
+              </CardBody>
+          </Card>
         </div>
 
-        <div id='contact' style={contactSection}>
-          <p className="text-lg-left font-weight-normal" style={contactText}>
-            Choose from a wide range of data cabling solutions, from one-off
-            cabling projects to on-going network infrastructure support.<br/><br/>
-            Contact us for a free quote.
-          </p>
+        <View>
+        <div id='contact' style={contactView}>
+          <Mask overlay="black-strong" className="flex-center" style={contactSection}>
+            <p className="white-text" style={contactText}>
+              Choose from a wide range of data cabling solutions, from one-off
+              cabling projects to on-going network infrastructure support.
+            </p>
+          </Mask>
           <Card style={contactCard}>
-            <form style={contactForm}>
-              <label htmlFor="defaultFormContactNameEx" className="grey-text">Your name</label>
-              <input type="text" id="defaultFormContactNameEx" className="form-control"/>
+            <form style={contactForm} onSubmit={this.sendMessage}>
+              <label htmlFor="name" className="grey-text">Your name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="form-control"
+                value={this.state.form.name}
+                onChange={this.handleChange}
+              />
               <br/>
-              <label htmlFor="defaultFormContactEmailEx" className="grey-text">Your email</label>
-              <input type="email" id="defaultFormContactEmailEx" className="form-control"/>
+              <label htmlFor="email" className="grey-text">Your email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                value={this.state.form.email}
+                onChange={this.handleChange}
+              />
               <br/>
-              <label htmlFor="defaultFormContactMessageEx" className="grey-text">Your message</label>
-              <textarea type="text" id="defaultFormContactMessageEx" className="form-control" rows="8"></textarea>
+              <label htmlFor="message" className="grey-text">Your message</label>
+              <textarea
+                type="text"
+                id="message"
+                name="message"
+                className="form-control"
+                rows="8"
+                value={this.state.form.message}
+                onChange={this.handleChange}
+              ></textarea>
               <div className="text-center mt-4">
-                <button className="btn btn-outline-warning" type="submit">Send<i className="fa fa-paper-plane-o ml-2"></i></button>
+                <button className="btn btn-outline-warning" type="submit" onClick={this.sendMessage}>Send<i className="fa fa-paper-plane-o ml-2"></i></button>
               </div>
             </form>
           </Card>
         </div>
+        </View>
+
+        <footer style={copyright}>
+            © 2018 Copyright - <a href="https://nconnect.io">NConnect</a>
+        </footer>
       </div>
     );
   }
